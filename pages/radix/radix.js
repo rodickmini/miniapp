1ab8989153b15ee234213d8eea6b433d36d560e9
caiyou <rodickcai@gmail.com>
2018-06-15 18:23:41 +0800

    UPDATE tab

---
+++ pages/radix/radix.js
@@ -0,0 +1,39 @@
+const memberService = require('../../service/member')
+
+Page({
+  onLoad: function() { },
+  userInfoCb: function(res) {
+    const userinfo = res.detail.userInfo
+    let self = this
+    wx.getLocation({
+      type: 'gcj02',
+      success: function (res) {
+        const [lat, lon] = [res.latitude, res.longitude]
+        wx.showLoading({
+          title: '搜寻中',
+        })
+        memberService.getMembers(lat, lon, userinfo).then(res => {
+          if(res.data.code === 0) {
+            wx.hideLoading()
+            wx.showToast({
+              title: '搜寻完毕！',
+              icon: 'success'
+            })
+            console.log(res.data.data.geohash)
+            self.setData({
+              memberList: res.data.data.memberList
+            })
+          } else {
+            console.log(res.err)
+          }
+        })
+      },
+      fail: function (res) {
+        console.log('fail')
+      },
+      complete: function (res) {
+        console.log('complete')
+      },
+    })
+  }
+})
