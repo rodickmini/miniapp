3ce99392f04cd9c11262ec2d9376440816713989
caiyou <rodickcai@gmail.com>
2018-07-16 10:54:28 +0800

    UPDATE

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,26 @@
 Page({
-  
+  openAlbum: function () {
+    wx.chooseImage({
+      count: 3,
+      sourceType: ['album'],
+      success: function (res) { 
+        console.log(res)
+      }
+    })
+  },
+  openCamera: function () {
+    wx.chooseImage({
+      count: 3,
+      sourceType: ['camera'],
+      success: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  onShareAppMessage: function () {
+    return {
+      title: '自定义转发标题',
+      path: '/pages/radix/radix'
+    }
+  }
 })
